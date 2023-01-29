81e9eec67a3f215b9a312ae257d4e3ddab87ecb2
Vishal Malhotra <vmalhotra362@gmail.com>
2023-01-29 17:52:19 +0530

    changes

--- index.js
+++ index.js
@@ -10,26 +10,9 @@ var arr = Array(60)
   .fill(null)
   .map(() => Array(0));
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-// Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
-
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
-
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.onText(/\/start/, async (msg) => {
-  // console.log(msg);
   const chatId = msg.chat.id;
   const date = msg.date;
 
@@ -40,7 +23,7 @@ bot.onText(/\/start/, async (msg) => {
   newdate = parseInt(newdate.substring(index1 + 1, index2));
 
   arr[newdate].push(chatId);
-  // console.log(arr[newdate]);
+
   let weather = await getCurrentWeather();
   bot.sendMessage(
     chatId,
@@ -49,15 +32,12 @@ bot.onText(/\/start/, async (msg) => {
 });
 
 const sendMessageToUser = async () => {
-  // let weather = await getCurrentWeather();
-  // console.log(weather);
   setInterval(async () => {
     let weather = await getCurrentWeather();
     // console.log(weather);
     var currentDa = new Date();
     var nowTime = new Date(currentDa.getTime());
     var Cminute = nowTime.getMinutes();
-    console.log(Cminute);
 
     arr[Cminute].forEach(sendmessage);
     function sendmessage(item) {
@@ -67,7 +47,6 @@ const sendMessageToUser = async () => {
       );
     }
   }, 1000 * 60);
-  console.log();
 };
 
 const app = express();
@@ -88,14 +67,3 @@ app.listen(process.env.PORT || 8000, async () => {
   console.log("app running on port,", process.env.PORT || 8000);
   sendMessageToUser();
 });
-
-//   from: {
-//     id: 1041539086,
-//     is_bot: false,
-//     first_name: 'Vishal',
-//     language_code: 'en'
-//   },
-//   chat: { id: 1041539086, first_name: 'Vishal', type: 'private' },
-//   date: 1674944153,
-//   text: 'Cb'
-// }
